c80bb082e9213408fbe93c680cc235bb0bc11fee
MicGul <mwarioxin@gmail.com>
2025-08-02 18:27:58 +0200

    Added initial connection to RabbitMQ local docker server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	rabbitConnectionString := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(rabbitConnectionString)
+	if err != nil {
+		log.Fatalf("Error connecting to RabbitMQ: %v", err)
+	}
+	defer connection.Close()
+
+	fmt.Println("Successfuly connected to RabbitMq server")
+
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("RabbitMQ connection closed.")
 }
